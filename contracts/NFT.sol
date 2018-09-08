2079c1086b0b2d3c59338a72a356a56808c03357
Michael <michael@x1-yoga.uwyo.edu>
2018-09-08 01:30:07 -0500

    added NFT

---
+++ contracts/NFT.sol
@@ -0,0 +1,31 @@
+pragma solidity ^0.4.24;
+
+import "zeppelin-solidity/contracts/token/ERC721/ERC721Token.sol";
+import "zeppelin-solidity/contracts/ownership/Ownable.sol";
+
+/**
+ * @title CryptoPuff
+ * CryptoPuff - a contract for my non-fungible crypto puffs.
+ */
+contract NFT is ERC721Token, Ownable {
+    constructor()  ERC721Token("CryptoPuff", "PUFF") public { }
+
+    /**
+    * @dev Mints a token to an address with a tokenURI.
+    * @param _to address of the future owner of the token
+    * @param _tokenURI token URI for the token
+    */
+    function mintTo(address _to, string _tokenURI) public onlyOwner {
+        uint256 newTokenId = _getNextTokenId();
+        _mint(_to, newTokenId);
+        _setTokenURI(newTokenId, _tokenURI);
+    }
+
+    /**
+    * @dev calculates the next token ID based on totalSupply
+    * @return uint256 for the next token ID
+    */
+    function _getNextTokenId() private view returns (uint256) {
+        return totalSupply().add(1); 
+    }
+}
